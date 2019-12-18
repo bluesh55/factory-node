9bf130b2f1bd42875fd4649cdc7a976fd1b7dc56
bluesh55 <seunghwan.oh5@gmail.com>
2019-12-18 18:42:54 +0900

    Support factory model written in typescript

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import path from 'path'
 import Factory from './factory'
 import config from './config'
 import { Specification } from './model'
+import * as ts from 'typescript'
+const _eval = require('node-eval')
 
 const loadResult = config.load()
 
@@ -14,17 +16,30 @@ export const factory = new Factory()
 
 fs.readdirSync(config.modelsDir)
   .filter(file => {
-    return file.indexOf('.') !== 0 && file.slice(-3) === '.js'
+    const extension = file.slice(-3)
+    return file.indexOf('.') !== 0 && ['.js', '.ts'].includes(extension)
   })
-  .forEach(file => {
-    const { name, specification, creator } = require(path.join(
-      config.modelsDir,
-      file
-    ))
+  .forEach(async file => {
+    const extension = file.slice(-3)
 
-    factory.addModel(
-      <string>name,
-      <Specification>specification,
-      <Function>creator
-    )
+    let model: {
+      name?: string
+      specification?: Specification
+      creator?: Function
+    } = {}
+
+    const modelFilePath = path.join(config.modelsDir, file)
+
+    if (extension === '.js') {
+      model = require(modelFilePath)
+    } else if (extension === '.ts') {
+      const tsFileContent = fs.readFileSync(modelFilePath, { encoding: 'utf8' })
+      const jsFileContent = ts.transpileModule(tsFileContent, {
+        compilerOptions: { module: ts.ModuleKind.CommonJS },
+      })
+      const modelModule = _eval(jsFileContent.outputText)
+      model = modelModule.default
+    }
+
+    factory.addModel(model.name!, model.specification!, model.creator!)
   })
